53516fdb368faf0d1f4fd9c9ce5531f2add94ec9
Dmytro Shtympel <dshtympel@reenbit.com>
2020-06-20 14:06:27 +0300

    Create Email sender

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!!!");
             Console.WriteLine("Hello World!");
         }
     }
